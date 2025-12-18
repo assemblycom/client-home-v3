4f4556573597e47888d6c12692f644b3208b309e
Rojan Rajbhandari <rrojan20@gmail.com>
2025-12-18 19:51:13 +0545

    chore(OUT-2821): polish error handling

--- src/errors/api-error.ts
+++ src/errors/api-error.ts
@@ -1,18 +1,13 @@
 import { BaseServerError } from '@/errors/base-server-error'
 
 class APIError extends BaseServerError {
-  readonly error?: unknown
-
   constructor(
     message: string,
     readonly status: number = 500,
-    readonly opts?: {
-      error?: unknown
-    },
+    readonly error?: unknown,
   ) {
     super(message, status)
     this.name = 'APIError'
-    this.error = opts?.error
   }
 }
 
