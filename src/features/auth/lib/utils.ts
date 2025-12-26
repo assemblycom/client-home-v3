b0ad64b49ff204e6c37f160c7b1059146b040d2d
Rojan Rajbhandari <rrojan20@gmail.com>
2025-12-26 14:17:01 +0545

    refactor(OUT-2834): organize all auth code in auth feature

--- src/features/auth/lib/utils.ts
+++ src/features/auth/lib/utils.ts
@@ -1,5 +1,7 @@
 import type { NextRequest } from 'next/server'
 import { v4 as uuidv4 } from 'uuid'
+import type { HttpMethod, RouteRule } from '@/app/routes'
+import { AuthenticatedAPIHeaders } from '@/app/types'
 
 /**
  * Prevent authorization bypass by injecting custom headers to request
@@ -9,11 +11,11 @@ import { v4 as uuidv4 } from 'uuid'
 export const getSanitizedHeaders = (req: NextRequest) => {
   const sanitizedHeaders = new Headers(req.headers)
 
-  sanitizedHeaders.delete('x-custom-app-token')
-  sanitizedHeaders.delete('x-internal-user-id')
-  sanitizedHeaders.delete('x-client-id')
-  sanitizedHeaders.delete('x-company-id')
-  sanitizedHeaders.delete('x-workspace-id')
+  sanitizedHeaders.delete(AuthenticatedAPIHeaders.CUSTOM_APP_TOKEN)
+  sanitizedHeaders.delete(AuthenticatedAPIHeaders.INTERNAL_USER_ID)
+  sanitizedHeaders.delete(AuthenticatedAPIHeaders.CLIENT_ID)
+  sanitizedHeaders.delete(AuthenticatedAPIHeaders.COMPANY_ID)
+  sanitizedHeaders.delete(AuthenticatedAPIHeaders.WORKSPACE_ID)
 
   // This is an idempotent request trace ID used to track the entire session req/resp cycles
   if (!sanitizedHeaders.get('x-req-trace-id')) {
@@ -21,4 +23,15 @@ export const getSanitizedHeaders = (req: NextRequest) => {
   }
 
   return sanitizedHeaders
+}
+
+function matches(rule: RouteRule, pathname: string, method: string) {
+  if (typeof rule === 'string') return rule === pathname
+  if (rule.path !== pathname) return false
+  if (!rule.methods) return true
+  return rule.methods.includes(method as HttpMethod)
+}
+
+export function isAuthorized(rules: RouteRule[], req: NextRequest) {
+  return rules.some((r) => matches(r, req.nextUrl.pathname, req.method))
 }
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,64 +1,14 @@
-import { AssemblyNoTokenError } from '@assembly/errors'
-import { authenticateToken } from '@auth/lib/authenticate'
-import { getSanitizedHeaders } from '@auth/lib/utils'
+import { authenticateProxy } from '@auth/lib/authenticate'
 import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
-import { authorizedRoutes, type HttpMethod, type RouteRule } from '@/app/routes'
-import { NotFoundError } from '@/errors/not-found.error'
-import { withErrorHandler } from '@/lib/with-error-handler'
-import { AuthenticatedAPIHeaders } from './app/types'
 
 /**
  * Application proxy that handles authentication and authorization for internal and client users
  */
-export const proxy = withErrorHandler(async (req: NextRequest) => {
-  const headers = getSanitizedHeaders(req)
-
-  // Handle public routes
-  if (isAuthorized(authorizedRoutes.public, req)) {
-    return NextResponse.next({ headers })
-  }
-
-  const isInternal = isAuthorized(authorizedRoutes.internalUsers, req)
-  const isClient = isAuthorized(authorizedRoutes.clientUsers, req)
-
-  if (!isInternal && !isClient) {
-    throw new NotFoundError()
-  }
-
-  const token = req.nextUrl.searchParams.get('token')
-  if (!token) {
-    throw new AssemblyNoTokenError()
-  }
-
-  const tokenPayload = await authenticateToken(token)
-
-  return NextResponse.next({
-    headers: {
-      ...headers,
-      [AuthenticatedAPIHeaders.CUSTOM_APP_TOKEN]: token,
-      [AuthenticatedAPIHeaders.INTERNAL_USER_ID]: tokenPayload.internalUserId,
-      [AuthenticatedAPIHeaders.CLIENT_ID]: tokenPayload.clientId,
-      [AuthenticatedAPIHeaders.COMPANY_ID]: tokenPayload.companyId,
-      [AuthenticatedAPIHeaders.WORKSPACE_ID]: tokenPayload.workspaceId,
-    },
-  })
-})
+export const proxy = async (req: NextRequest) => {
+  return await authenticateProxy(req)
+}
 
 export const config = {
   // Run middleware on all routes except Next internals and static files
   matcher: '/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)',
-}
-
-// --- Helpers ---
-
-function matches(rule: RouteRule, pathname: string, method: string) {
-  if (typeof rule === 'string') return rule === pathname
-  if (rule.path !== pathname) return false
-  if (!rule.methods) return true
-  return rule.methods.includes(method as HttpMethod)
-}
-
-function isAuthorized(rules: RouteRule[], req: NextRequest) {
-  return rules.some((r) => matches(r, req.nextUrl.pathname, req.method))
 }
