89ecc7c730890accbfb7b6adb8e56f2ab0fdeae9
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-07 09:15:52 +0545

    feat(OUT-2824): add modular headings extension

---
+++ src/features/editor/components/Editor/extensions/Headings.ext.ts
@@ -0,0 +1,28 @@
+import { Extension } from '@tiptap/core'
+
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    headingCommands: {
+      setHeading1: () => ReturnType
+      setHeading2: () => ReturnType
+    }
+  }
+}
+
+export const HeadingCommands = Extension.create({
+  name: 'headingCommands',
+
+  addCommands() {
+    return {
+      setHeading1:
+        () =>
+        ({ chain }) =>
+          chain().setNode('heading', { level: 1 }).run(),
+
+      setHeading2:
+        () =>
+        ({ chain }) =>
+          chain().setNode('heading', { level: 2 }).run(),
+    }
+  },
+})
