1a8d77af73eeba490abe54c2c18d6a075b716b96
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-09 16:34:03 +0545

    refactor(OUT-2893): clean code

--- src/features/editor/components/Editor/extensions/index.ts
+++ src/features/editor/components/Editor/extensions/index.ts
@@ -1,5 +1,18 @@
-export * from './Headings.ext'
-export * from './Placeholder.ext'
-export * from './SlashCommands.ext'
-export * from './StarterKit.ext'
-export * from './Table.ext'
+import { CalloutExt } from '@editor/components/Editor/extensions/Callout.ext'
+import { Headings } from '@editor/components/Editor/extensions/Headings.ext'
+import { PlaceholderExt } from '@editor/components/Editor/extensions/Placeholder.ext'
+import { SlashCommandsExt } from '@editor/components/Editor/extensions/SlashCommands.ext'
+import { StarterKitExt } from '@editor/components/Editor/extensions/StarterKit.ext'
+import { TableExt } from '@editor/components/Editor/extensions/Table.ext'
+import type { Extension, Node } from '@tiptap/core'
+
+const extensions: Array<Extension | Node> = [
+  StarterKitExt,
+  Headings,
+  CalloutExt,
+  TableExt,
+  PlaceholderExt,
+  SlashCommandsExt,
+]
+
+export default extensions
