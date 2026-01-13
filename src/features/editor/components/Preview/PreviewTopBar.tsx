7682e0576298c80e0439f3cfac8ce0df89e51528
priosshrsth <shrsthprios@hotmail.com>
2026-01-13 12:46:18 +0000

    fix(out-2912): fix dots color

--- src/features/editor/components/Preview/PreviewTopBar.tsx
+++ src/features/editor/components/Preview/PreviewTopBar.tsx
@@ -6,11 +6,11 @@ interface PreviewTopBarProps {
 
 export const PreviewTopBar: React.FC<PreviewTopBarProps> = ({ url }) => {
   return (
-    <div className="grid grid-cols-[3.25rem_1fr_3.25rem] items-center justify-between gap-2.5 px-5 py-2">
+    <div className="grid grid-cols-[3.25rem_1fr_3.25rem] items-center justify-between gap-2.5 bg-white px-5 py-2">
       <div className="flex gap-1.5">
-        <div className="h-3 w-3 rounded-full bg-neutral" />
-        <div className="h-3 w-3 rounded-full bg-neutral" />
-        <div className="h-3 w-3 rounded-full bg-neutral" />
+        <div className="h-3 w-3 overflow-clip rounded-full bg-neutral-200" />
+        <div className="h-3 w-3 rounded-full bg-neutral-200" />
+        <div className="h-3 w-3 rounded-full bg-neutral-200" />
       </div>
       <div className="flex justify-center">
         <div className="line-clamp-1 w-full max-w-md truncate text-ellipsis rounded-md bg-neutral-100 px-3 pt-1.25 pb-1 text-center text-gray-300 text-xs">
