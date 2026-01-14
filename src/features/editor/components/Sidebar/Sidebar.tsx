c2b8577715f81aedec23eb5242ed00e0892e8daf
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-14 12:39:11 +0545

    feat(OUT-2838): add sidebar components

---
+++ src/features/editor/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,49 @@
+import { Accordion } from '@editor/components/Sidebar/Accordion'
+import { Actions } from '@editor/components/Sidebar/Actions'
+import { BackgroundColor } from '@editor/components/Sidebar/BackgroundColor'
+import { Banner } from '@editor/components/Sidebar/Banner'
+import { DynamicFields } from '@editor/components/Sidebar/DynamicFields'
+import { Segment } from '@editor/components/Sidebar/Segment'
+import { cn } from '@/utils/tailwind'
+
+interface SidebarProps {
+  className?: string
+}
+
+const AccordionItems = [
+  {
+    title: 'Banner',
+    content: <Banner />,
+  },
+  {
+    title: 'Actions',
+    content: <Actions />,
+  },
+  {
+    title: 'Dynamic fields',
+    content: <DynamicFields />,
+  },
+  {
+    title: 'Background color',
+    content: <BackgroundColor />,
+  },
+  {
+    title: 'Segments',
+    content: <Segment />,
+  },
+]
+
+export const Sidebar = ({ className }: SidebarProps) => {
+  return (
+    <aside className={cn('h-screen overflow-y-auto border-border-gray border-l', className)}>
+      <div className="box-content flex h-14 items-center border-border-gray border-b px-6 text-custom-xl">
+        Customization
+      </div>
+      <div className="flex flex-col py-5">
+        {AccordionItems.map((item) => (
+          <Accordion key={item.title} title={item.title} content={item.content} className="pr-5 pl-6" />
+        ))}
+      </div>
+    </aside>
+  )
+}
