eea688a88cf8bed043bb4622dda7a784771e5b7f
priosshrsth <shrsthprios@hotmail.com>
2026-01-14 07:27:48 +0000

    feat(out-2925): add display mode toggle

--- src/features/editor/components/Preview/Preview.tsx
+++ src/features/editor/components/Preview/Preview.tsx
@@ -1,14 +1,17 @@
 import { useAuthStore } from '@auth/providers/auth.provider'
+import { DisplayMode, useViewStore } from '@editor/stores/viewStore'
 import { useQuery } from '@tanstack/react-query'
 import { ROUTES } from '@/app/routes'
 import { Loader } from '@/features/common/components/Loader/Loader'
 import type { WorkspaceResponse } from '@/lib/assembly/types'
 import { api } from '@/lib/core/axios.instance'
+import { cn } from '@/utils/tailwind'
 import { PreviewTopBar } from './PreviewTopBar'
 
 export function Preview() {
   const workspaceId = useAuthStore((store) => store.workspaceId)
   const token = useAuthStore((store) => store.token)
+  const displayMode = useViewStore((store) => store.displayMode)
 
   const { data, isLoading, error } = useQuery({
     queryKey: ['workspace', workspaceId],
@@ -17,7 +20,14 @@ export function Preview() {
   })
 
   return (
-    <div className="flex h-full flex-col overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm">
+    <div
+      className={cn(
+        'mx-auto flex h-full flex-col overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm',
+        {
+          'max-w-sm': displayMode === DisplayMode.MOBILE,
+        },
+      )}
+    >
       <PreviewTopBar url={data?.portalUrl} />
 
       <div className="flex min-h-32 flex-1 flex-col items-center justify-center p-6">
