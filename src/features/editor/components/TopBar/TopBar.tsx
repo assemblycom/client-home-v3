6039bf5fc91b3044c936cd3b1308ea3edd408494
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-14 12:39:30 +0545

    feat(OUT-2840): add topbar component

---
+++ src/features/editor/components/TopBar/TopBar.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { TabBtn } from '@editor/components/TopBar/TabBtn'
+import { useViewStore, ViewMode } from '@editor/stores/viewStore'
+import { useEffect, useEffectEvent, useMemo } from 'react'
+import { debounce } from '@/utils/debounce'
+
+export const TopBar = () => {
+  const viewMode = useViewStore((store) => store.viewMode)
+  const changeView = useViewStore((store) => store.changeView)
+
+  const changeViewInStore = useEffectEvent((viewMode: ViewMode) => changeView({ viewMode }))
+
+  const debouncedChangeView = useMemo(() => debounce((view: ViewMode) => changeViewInStore(view), 250), [])
+
+  useEffect(() => {
+    return () => {
+      debouncedChangeView.cancel()
+    }
+  }, [debouncedChangeView])
+
+  const changeViewMode = (view: ViewMode) => {
+    debouncedChangeView(view)
+  }
+
+  return (
+    <nav className="flex min-h-8 cursor-default items-center justify-between border-border-gray border-b px-5 py-3">
+      <div className="flex items-center gap-2 rounded-sm p-1 outline outline-border-gray">
+        <TabBtn
+          label="Editor"
+          active={viewMode === ViewMode.EDITOR}
+          handleClick={() => changeViewMode(ViewMode.EDITOR)}
+        />
+        <TabBtn
+          label="Preview"
+          active={viewMode === ViewMode.PREVIEW}
+          handleClick={() => changeViewMode(ViewMode.PREVIEW)}
+        />
+      </div>
+    </nav>
+  )
+}
