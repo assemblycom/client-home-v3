dc47daff5db75475307ebb9306964bc45766240b
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-14 18:52:21 +0545

    feat(OUT-2928): add and configure Image + Filehandler

---
+++ src/features/editor/components/Editor/extensions/Image.ext.ts
@@ -0,0 +1,10 @@
+import Image from '@tiptap/extension-image'
+
+export const ImageExt = Image.configure({
+  resize: {
+    enabled: true,
+    alwaysPreserveAspectRatio: true,
+    minHeight: 50,
+    minWidth: 50,
+  },
+})
--- src/features/editor/components/Editor/extensions/extensions.ts
+++ src/features/editor/components/Editor/extensions/extensions.ts
@@ -1,6 +1,9 @@
 import { CalloutExt } from '@extensions/Callout.ext'
+import { DropcursorExt } from '@extensions/Dropcursor.ext'
 import { EmbedExt } from '@extensions/Embed.ext'
+import { FileHandlerExt } from '@extensions/FileHandler.ext'
 import { Headings } from '@extensions/Headings.ext'
+import { ImageExt } from '@extensions/Image.ext'
 import { PlaceholderExt } from '@extensions/Placeholder.ext'
 import { SlashCommandsExt } from '@extensions/SlashCommands.ext'
 import { StarterKitExt } from '@extensions/StarterKit.ext'
@@ -14,6 +17,9 @@ const extensions: Array<Extension | Node> = [
   TableExt,
   PlaceholderExt,
   EmbedExt,
+  ImageExt,
+  FileHandlerExt,
+  DropcursorExt,
   SlashCommandsExt,
 ]
 
