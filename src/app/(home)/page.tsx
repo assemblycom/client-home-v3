e55bbe763732942101a9acdd6284aa40067736a0
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-15 17:56:28 +0545

    feat(OUT-2871): prefetch settings on server

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,8 @@
-import { EditorWrapper } from '@editor/components/EditorWrapper'
+import { Loader } from '@common/components/Loader'
+import { EditorSection } from '@editor/components/EditorSection'
 import { Sidebar } from '@editor/components/Sidebar'
 import { TopBar } from '@editor/components/TopBar'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
@@ -8,7 +10,9 @@ export default function Home() {
       <div className="flex-1">
         <TopBar />
         <div className="h-[calc(100vh-64px)] overflow-y-scroll bg-background-primary px-6 py-6.5">
-          <EditorWrapper />
+          <Suspense fallback={<Loader />}>
+            <EditorSection />
+          </Suspense>
         </div>
       </div>
       <Sidebar className="w-1/3 max-w-[394]" />
