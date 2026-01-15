631a5c31b0dde1e5698ddf064bcf26e8fa9559f8
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-15 17:53:09 +0545

    refactor(OUT-2869): clean code

---
+++ src/features/common/providers/app.provider.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { getQueryClient } from '@/lib/core/query.utils'
+
+export function AppProvider({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
