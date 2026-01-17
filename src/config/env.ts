2682b4c3666d8e276ad4125085f54eb752fd8704
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-17 09:08:41 +0545

    chore(OUT-2932): add env vars for supabase

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,8 +4,14 @@ import { z } from 'zod'
 
 const EnvSchema = z.object({
   ASSEMBLY_API_KEY: z.string().min(1),
+
+  VERCEL_ENV: z.enum(['production', 'preview', 'development']).optional(), // NOTE: We can add other custom environments here
+
   DATABASE_URL: z.url(),
-  VERCEL_ENV: z.string().optional(),
+  SUPABASE_URL: z.url(),
+  SUPABASE_PUBLISHABLE_KEY: z.string().min(1),
+  SUPABASE_BUCKET_NAME: z.string().min(1),
+  SUPABASE_SIGNED_URL_EXPIRY: z.number().optional().default(300),
 })
 
 export default EnvSchema.parse(process.env)
