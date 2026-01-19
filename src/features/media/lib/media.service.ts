081e9791fce87763a502a39e6956cb5307458c07
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-19 18:57:52 +0545

    feat(OUT-2832): add endpoint to get signedUrl for file

--- src/features/media/lib/media.service.ts
+++ src/features/media/lib/media.service.ts
@@ -5,16 +5,19 @@ import type { User } from '@auth/lib/user.entity'
 import { MediaFolders } from '@media/constants'
 import type { MediaRepository } from '@media/lib/media.repository'
 import MediaDrizzleRepository from '@media/lib/media.repository'
-import type { MediaUploadResponseDto } from '@media/media.dto'
-import env from '@/config/env'
+import type { MediaSignedUrlResponseDto } from '@media/media.dto'
+import httpStatus from 'http-status'
 import db from '@/db'
+import APIError from '@/errors/api.error'
 import BaseService from '@/lib/core/base.service'
 import SupabaseClient from '@/lib/supabase/supabase-server-client'
+import { getFileNameWithTimestamp } from '@/utils/supabase'
 
 export default class MediaService extends BaseService {
   constructor(
     protected readonly user: User,
     protected readonly assembly: AssemblyClient,
+    protected readonly supabase: SupabaseClient,
     protected readonly repository: MediaRepository,
   ) {
     super(user, assembly)
@@ -25,19 +28,31 @@ export default class MediaService extends BaseService {
    */
   static new(user: User): MediaService {
     const assembly = new AssemblyClient(user.token)
+    const supabase = new SupabaseClient()
     const repository = new MediaDrizzleRepository(db)
-    return new MediaService(user, assembly, repository)
+    return new MediaService(user, assembly, supabase, repository)
   }
 
-  async getSignedUrl(fileName: string, mode: MediaFolders = MediaFolders.EDITOR): Promise<MediaUploadResponseDto> {
-    const supabase = new SupabaseClient()
+  async getSignedFileUrl(filePath: string): Promise<string> {
+    const signedUrlInfo = await this.supabase.getSignedUrls([filePath])
+    if (signedUrlInfo[0].error) {
+      throw new APIError(signedUrlInfo[0].error, httpStatus.BAD_REQUEST)
+    }
+    return signedUrlInfo[0].signedUrl
+  }
+
+  async getSignedUploadUrl(
+    fileName: string,
+    mode: MediaFolders = MediaFolders.EDITOR,
+  ): Promise<MediaSignedUrlResponseDto> {
     const path = this.getFileUploadPath(fileName, mode)
 
-    const signedUrlInfo = await supabase.getSignedUploadUrl(path)
+    const signedUrlInfo = await this.supabase.getSignedUploadUrl(path)
     return signedUrlInfo
   }
 
   private getFileUploadPath(fileName: string, mode: MediaFolders): string {
-    return `${env.SUPABASE_BUCKET_NAME}/${this.user.workspaceId}/${mode}/${fileName}_${Date.now()}`
+    const fileNameWithTimestamp = getFileNameWithTimestamp(fileName)
+    return `${this.user.workspaceId}/${mode}/${fileNameWithTimestamp}`
   }
 }
--- src/features/media/media.dto.ts
+++ src/features/media/media.dto.ts
@@ -1,13 +1,18 @@
 import z from 'zod'
 
+export const MediaSignedFileUrlRequestDtoSchema = z.object({
+  filePath: z.string().min(1),
+})
+export type MediaSignedFileUrlRequestDto = z.infer<typeof MediaSignedFileUrlRequestDtoSchema>
+
 export const MediaUploadRequestDtoSchema = z.object({
   fileName: z.string().min(1),
 })
 export type MediaUploadRequestDto = z.infer<typeof MediaUploadRequestDtoSchema>
 
-export const MediaUploadResponseDtoSchema = z.object({
+export const MediaSignedUrlResponseDtoSchema = z.object({
   signedUrl: z.string().min(1),
   token: z.string().min(1),
   path: z.string().min(1),
 })
-export type MediaUploadResponseDto = z.infer<typeof MediaUploadResponseDtoSchema>
+export type MediaSignedUrlResponseDto = z.infer<typeof MediaSignedUrlResponseDtoSchema>
