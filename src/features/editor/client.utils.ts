baf231c5a8c36918c7e3aa1f098f61527d9bcf81
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-19 18:59:15 +0545

    feat(OUT-2932): add client side utils for editor

---
+++ src/features/editor/client.utils.ts
@@ -0,0 +1,57 @@
+import 'client-only'
+
+import type { MediaSignedUrlResponseDto } from '@media/media.dto'
+import type { Editor } from '@tiptap/core'
+import { api } from '@/lib/core/axios.instance'
+
+export const uploadFileToSupabase = async (file: File, token: string): Promise<string> => {
+  const signedUrlResponse = await api.post<{ data: MediaSignedUrlResponseDto }>(`/api/media?token=${token}`, {
+    fileName: file.name,
+  })
+
+  const signedUrl = signedUrlResponse.data.data.signedUrl
+
+  const res = await fetch(signedUrl, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': file.type,
+    },
+    body: file,
+  })
+
+  if (!res.ok) {
+    const text = await res.text().catch(() => '')
+    console.error('Upload failed:', res.status, text)
+  }
+  const filePath = (await res.json()).Key
+
+  const signedImgUrlResponse = await api.get<{ data: MediaSignedUrlResponseDto }>(`/api/media?token=${token}`, {
+    params: { filePath },
+  })
+
+  return signedImgUrlResponse.data.data.signedUrl
+}
+
+export const replaceEditorImageSrcByUploadId = (editor: Editor, uploadId: string, newSrc: string) => {
+  const { state, view } = editor
+
+  let tr = state.tr
+  let found = false
+
+  state.doc.descendants((node, pos) => {
+    if (node.type.name === 'image' && node.attrs['data-upload-id'] === uploadId) {
+      tr = tr.setNodeMarkup(pos, undefined, {
+        ...node.attrs,
+        src: newSrc,
+        'data-upload-id': null, // cleanup attr
+      })
+      found = true
+      return false // stop traversal
+    }
+    return true
+  })
+
+  if (found) {
+    view.dispatch(tr)
+  }
+}
