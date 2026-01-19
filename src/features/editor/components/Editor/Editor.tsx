cd03d243b538e5b3a844d086c7e4cbd3a9e5f770
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-19 20:07:22 +0545

    fix(OUT-2932): quickfix for image src updates

--- src/features/editor/components/Editor/Editor.tsx
+++ src/features/editor/components/Editor/Editor.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { useAuthStore } from '@auth/providers/auth.provider'
+import { replaceEditorImageSrcByUploadId, uploadFileToSupabase } from '@editor/client.utils'
 import { BubbleMenu } from '@editor/components/Editor/BubbleMenu'
 import { useAppControls } from '@editor/hooks/useAppControls'
 import { useEditorStore } from '@editor/stores/editorStore'
 import { EmbedBubbleInput } from '@extensions/Embed.ext/EmbedBubbleInput'
 import extensions from '@extensions/extensions'
+import { FileHandlerExt } from '@extensions/FileHandler.ext'
 import { useSettingsStore } from '@settings/providers/settings.provider'
-import { EditorContent, useEditor } from '@tiptap/react'
+import { EditorContent, useEditor, useEditorState } from '@tiptap/react'
 import { useEffect } from 'react'
 import { useShallow } from 'zustand/shallow'
 
@@ -29,7 +31,48 @@ export const Editor = ({ editable = true }: EditorProps) => {
   )
 
   const editor = useEditor({
-    extensions,
+    extensions: [
+      ...extensions,
+      FileHandlerExt.configure({
+        onPaste: (currentEditor, files, htmlContent) => {
+          files.forEach((file) => {
+            if (htmlContent) {
+              // if there is htmlContent, stop manual insertion & let other extensions handle insertion via inputRule
+              return
+            }
+
+            const fileReader = new FileReader()
+
+            fileReader.readAsDataURL(file)
+            fileReader.onload = async () => {
+              const randId = crypto.randomUUID()
+
+              currentEditor
+                .chain()
+                .focus()
+                .insertContentAt(currentEditor.state.selection.anchor, {
+                  type: 'image',
+                  attrs: {
+                    src: fileReader.result,
+                    title: randId,
+                  },
+                })
+                .focus()
+                .run()
+              setContent(currentEditor.getHTML())
+              const token = currentEditor.storage.token.token
+              if (!token) {
+                console.error('Could not upload to supabase due to missing token')
+                return // Keep the blob image for now
+              }
+
+              const signedUrl = await uploadFileToSupabase(file, token)
+              setContent(currentEditor.getHTML().replaceAll(fileReader.result as string, signedUrl))
+            }
+          })
+        },
+      }),
+    ],
     content,
     editable,
     immediatelyRender: false, // Avoid SSR & hydration issues
@@ -39,7 +82,6 @@ export const Editor = ({ editable = true }: EditorProps) => {
       },
     },
     onCreate({ editor }) {
-      // @ts-expect-error storage is not strictly typed right now
       editor.storage.token.token = token
     },
     onUpdate({ editor }) {
