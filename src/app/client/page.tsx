4582e31232445bcb6fbafa4b48e2d423d15bdaa3
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-26 14:33:34 +0545

    feat(OUT-2826): add client page

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,3 +1,9 @@
+import { ClientPreview } from '@editor/components/Editor/ClientPreview'
+
 export default function ClientPage() {
-  return <div>Client Page</div>
+  return (
+    <div className="flex h-screen w-screen">
+      <ClientPreview />
+    </div>
+  )
 }
