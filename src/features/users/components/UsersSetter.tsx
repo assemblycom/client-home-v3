e53a32c1ea346d17155afe6cd6178e74084fdfe5
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-29 01:51:45 +0545

    perf(OUT-2991): prevent unnecessary setting of users state obj

--- src/features/users/components/UsersSetter.tsx
+++ src/features/users/components/UsersSetter.tsx
@@ -2,14 +2,32 @@
 
 import { useUsersStore } from '@users/stores/usersStore'
 import type { UsersDto } from '@users/users.dto'
+import { useEffect } from 'react'
+import { getArraySymmetricDifference as getSymmetricDifference } from '@/utils/array'
 
 interface UsersSetterProps {
   users: UsersDto[]
 }
 
 export const UsersSetter = ({ users }: UsersSetterProps) => {
+  const prevUsers = useUsersStore((state) => state.users)
   const setUsers = useUsersStore((state) => state.setUsers)
-  setUsers(users)
+
+  useEffect(() => {
+    if (!users.length || prevUsers.length > 0) return
+
+    // NOTE: Might be unnecessary. keeping this just in case.
+    // Prevent infinite re-render by comparing if new users have been added or removed
+    // if (
+    //   getSymmetricDifference(
+    //     prevUsers.map((u) => u.id),
+    //     users.map((u) => u.id),
+    //   ).length === 0
+    // )
+    //   return
+
+    setUsers(users)
+  }, [users, setUsers, prevUsers])
 
   return null
 }
