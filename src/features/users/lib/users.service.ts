87c2c36fa3ffe9ae025c56d8514b63624fd1e984
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-29 19:46:12 +0545

    feat(OUT-2990): return clients & companies separately from api

--- src/features/users/lib/users.service.ts
+++ src/features/users/lib/users.service.ts
@@ -2,7 +2,7 @@ import AssemblyClient from '@assembly/assembly-client'
 import { MAX_FETCH_ASSEMBLY_RESOURCES } from '@assembly/constants'
 import type { ClientResponse, CompanyResponse } from '@assembly/types'
 import type { User } from '@auth/lib/user.entity'
-import type { UsersDto } from '@users/users.dto'
+import { type ClientsDto, type CompaniesDto, type UsersDto, UsersDtoSchema } from '@users/users.dto'
 import { HttpStatusCode } from 'axios'
 import APIError from '@/errors/api.error'
 import BaseService from '@/lib/core/base.service'
@@ -20,7 +20,7 @@ export default class UsersService extends BaseService {
     return new UsersService(user, assembly)
   }
 
-  async getClients(): Promise<UsersDto[]> {
+  async getClients(): Promise<UsersDto> {
     const [clients, companies] = await Promise.all([
       this.assembly.getClients({ limit: MAX_FETCH_ASSEMBLY_RESOURCES }),
       this.assembly.getCompanies({ limit: MAX_FETCH_ASSEMBLY_RESOURCES }),
@@ -30,43 +30,48 @@ export default class UsersService extends BaseService {
 
     // NOTE: Do not check for !companies || !companies.data since companies can be disabled in workspace
 
-    const companiesMap = new Map(companies.data?.map((company) => [company.id, company]))
-
-    const flattenedClients: UsersDto[] = []
+    const flattenedClients: ClientsDto[] = []
     for (const client of clients.data) {
       const companyCount = client.companyIds?.length || 0
       if (companyCount > 1) {
         // biome-ignore lint/style/noNonNullAssertion: companyIds must exist when companyCount > 1
         for (const companyId of client.companyIds!) {
-          flattenedClients.push(this.getParsedClientData(client, companiesMap.get(companyId)))
+          flattenedClients.push(this.getParsedClientData(client, companyId))
         }
       } else {
         const companyId = client.companyIds?.[0]
-        flattenedClients.push(this.getParsedClientData(client, companyId ? companiesMap.get(companyId) : undefined))
+        flattenedClients.push(this.getParsedClientData(client, companyId))
       }
     }
 
-    return flattenedClients.sort((a, b) => {
-      const aName = `${a.firstName} ${a.lastName || ''}`
-      const bName = `${b.firstName} ${b.lastName || ''}`
-      return aName.localeCompare(bName)
+    return UsersDtoSchema.parse({
+      clients: flattenedClients.sort((a, b) => {
+        const aName = `${a.firstName} ${a.lastName || ''}`
+        const bName = `${b.firstName} ${b.lastName || ''}`
+        return aName.localeCompare(bName)
+      }),
+      companies: companies.data?.map(this.getParsedCompaniesData) || [],
     })
   }
 
-  private getParsedClientData(client: ClientResponse, company?: CompanyResponse): UsersDto {
+  private getParsedClientData(client: ClientResponse, companyId?: string): ClientsDto {
     return {
       id: client.id,
       firstName: client.givenName,
       lastName: client.familyName,
       email: client.email,
       customFields: client.customFields,
-      company: company
-        ? {
-            id: company.id,
-            name: company.name,
-            customFields: company.customFields || {},
-          }
-        : null,
+      companyId,
+      avatarSrc: client.avatarImageUrl || undefined,
+      avatarFallbackColor: client.fallbackColor || undefined,
+    }
+  }
+
+  private getParsedCompaniesData(company: CompanyResponse): CompaniesDto {
+    return {
+      id: company.id,
+      name: company.name,
+      customFields: company.customFields || {},
     }
   }
 }
