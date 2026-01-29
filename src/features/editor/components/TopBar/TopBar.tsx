af12f28b6ae3cfdd2cbd609618d9f4a71354f6df
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-29 19:46:22 +0545

    feat(OUT-2990): add user company selector

--- src/features/editor/components/TopBar/TopBar.tsx
+++ src/features/editor/components/TopBar/TopBar.tsx
@@ -3,16 +3,47 @@
 import { DisplayModeTab } from '@editor/components/TopBar/DisplayModeTab'
 import { TabBtn } from '@editor/components/TopBar/TabBtn'
 import { useViewStore, ViewMode } from '@editor/stores/viewStore'
+import { useUsersStore } from '@users/stores/usersStore'
+import { UserCompanySelector, type UserCompanySelectorProps } from 'copilot-design-system'
 import { useEffect, useEffectEvent, useMemo } from 'react'
 import { debounce } from '@/utils/debounce'
 
 export const TopBar = () => {
   const viewMode = useViewStore((store) => store.viewMode)
-  const changeView = useViewStore((store) => store.updateView)
+  const changeView = useViewStore((store) => store.changeView)
+  const clients = useUsersStore((store) => store.clients)
+  const companies = useUsersStore((store) => store.companies)
+
+  const selectorClients = useMemo(
+    () =>
+      clients.map(
+        (client) =>
+          ({
+            ...client,
+            value: client.id,
+            label: `${client.firstName} ${client.lastName}`,
+            type: 'client',
+          }) satisfies UserCompanySelectorProps['clientUsers'][0],
+      ),
+    [clients],
+  )
+  const selectorCompanies = useMemo(
+    () =>
+      companies.map(
+        (company) =>
+          ({
+            ...company,
+            value: company.id,
+            label: company.name,
+            type: 'company',
+          }) satisfies UserCompanySelectorProps['companies'][0],
+      ),
+    [companies],
+  )
 
   const changeViewInStore = useEffectEvent((viewMode: ViewMode) => changeView({ viewMode }))
 
-  const debouncedChangeView = useMemo(() => debounce((view: ViewMode) => changeViewInStore(view), 250), [])
+  const debouncedChangeView = useMemo(() => debounce((view: ViewMode) => changeViewInStore(view), 100), [])
 
   useEffect(() => {
     return () => {
@@ -37,6 +68,14 @@ export const TopBar = () => {
           active={viewMode === ViewMode.PREVIEW}
           handleClick={() => changeViewMode(ViewMode.PREVIEW)}
         />
+        <div>
+          <UserCompanySelector
+            clientUsers={selectorClients}
+            companies={selectorCompanies}
+            name="pd"
+            onChange={() => null}
+          />
+        </div>
       </div>
       <div>{viewMode === ViewMode.PREVIEW && <DisplayModeTab />}</div>
     </nav>
