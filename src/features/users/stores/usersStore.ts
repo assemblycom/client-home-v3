b48af39750db15324b0b290dffcf1a36708dc05f
Rojan Rajbhandari <rrojan20@gmail.com>
2026-01-29 16:29:20 +0545

    feat(OUT-2991): add storage for client & companyId accounting for disabled companies

--- src/features/users/stores/usersStore.ts
+++ src/features/users/stores/usersStore.ts
@@ -2,18 +2,26 @@ import type { UsersDto } from '@users/users.dto'
 import { create } from 'zustand'
 
 interface UsersState {
+  previewClientId: string | null
+  previewCompanyId: string | null
   users: UsersDto[]
 }
 
 interface UsersActions {
   setUsers: (users: UsersDto[]) => void
+  setPreviewClientId: (clientId: string) => void
+  setPreviewCompanyId: (companyId: string) => void
 }
 
 const initialState: UsersState = {
   users: [],
+  previewClientId: null,
+  previewCompanyId: null,
 }
 
 export const useUsersStore = create<UsersState & UsersActions>()((set) => ({
   ...initialState,
   setUsers: (users: UsersDto[]) => set({ users }),
+  setPreviewClientId: (clientId: string) => set({ previewClientId: clientId }),
+  setPreviewCompanyId: (companyId: string) => set({ previewCompanyId: companyId }),
 }))
