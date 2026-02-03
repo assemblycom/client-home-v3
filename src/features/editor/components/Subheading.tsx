f3280066b78e3b9a31c8df75e1cb89b91315fb4f
Rojan Rajbhandari <rrojan20@gmail.com>
2026-02-03 14:15:00 +0545

    feat(OUT-2949): add simple subheading input

---
+++ src/features/editor/components/Subheading.tsx
@@ -0,0 +1,12 @@
+export const Subheading = () => {
+  return (
+    <div className="pt-2">
+      <input
+        type="text"
+        id="subheading"
+        placeholder="Subheader"
+        className="w-full border-none text-sm text-text-secondary leading-5.5 outline-none"
+      />
+    </div>
+  )
+}
