937de59c4e80a19369d94aa4e18984c1854b63d5
arpandhakal <arpandhakal20@gmail.com>
2026-02-09 19:45:33 +0545

    fix(OUT-3049): added settings on image extension to support width and height attribute on resize

--- src/features/editor/components/Editor/extensions/Image.ext.ts
+++ src/features/editor/components/Editor/extensions/Image.ext.ts
@@ -1,6 +1,36 @@
 import Image from '@tiptap/extension-image'
 
-export const ImageExt = Image.configure({
+export const ImageExt = Image.extend({
+  addAttributes() {
+    return {
+      ...this.parent?.(),
+      width: {
+        default: null,
+        parseHTML: (element) => element.getAttribute('width'),
+        renderHTML: (attributes) => {
+          if (!attributes.width) {
+            return {}
+          }
+          return {
+            width: attributes.width,
+          }
+        },
+      },
+      height: {
+        default: null,
+        parseHTML: (element) => element.getAttribute('height'),
+        renderHTML: (attributes) => {
+          if (!attributes.height) {
+            return {}
+          }
+          return {
+            height: attributes.height,
+          }
+        },
+      },
+    }
+  },
+}).configure({
   resize: {
     enabled: true,
     alwaysPreserveAspectRatio: true,
