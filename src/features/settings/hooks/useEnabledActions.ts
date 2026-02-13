939a22819a825bef080187372aba6ba507340dee
Rojan Rajbhandari <rrojan20@gmail.com>
2026-02-13 12:07:09 +0545

    feat(OUT-3139): add support for reordering support for actions

--- src/features/settings/hooks/useEnabledActions.ts
+++ src/features/settings/hooks/useEnabledActions.ts
@@ -6,10 +6,15 @@ export function useEnabledActions() {
   const actions = useSettingsStore((s) => s.actions)
 
   const enabledActions = useMemo(() => {
+    const order = actions.order ?? []
     // TODO:- remove type casting once files has been added in settings table
-    return Object.values(ActionDefinitions).filter(
-      (definition) => !!actions[definition.key as unknown as keyof typeof actions],
-    )
+    return Object.values(ActionDefinitions)
+      .filter((definition) => !!actions[definition.key as unknown as keyof typeof actions])
+      .sort((a, b) => {
+        const aIndex = order.indexOf(a.key)
+        const bIndex = order.indexOf(b.key)
+        return (aIndex === -1 ? Infinity : aIndex) - (bIndex === -1 ? Infinity : bIndex)
+      })
   }, [actions])
 
   return { enabledActions }
--- src/features/settings/lib/actions/types.ts
+++ src/features/settings/lib/actions/types.ts
@@ -1,10 +1,16 @@
 import { actions } from '@settings/lib/actions/actions.schema'
 import { createInsertSchema, createSelectSchema, createUpdateSchema } from 'drizzle-zod'
-import type z from 'zod'
+import { z } from 'zod'
 
-export const ActionsSchema = createSelectSchema(actions)
+const orderSchema = z.array(z.string())
 
-export const ActionsCreateSchema = createInsertSchema(actions).omit({
+export const ActionsSchema = createSelectSchema(actions, {
+  order: orderSchema,
+})
+
+export const ActionsCreateSchema = createInsertSchema(actions, {
+  order: orderSchema.optional(),
+}).omit({
   id: true,
   workspaceId: true,
   createdAt: true,
@@ -12,7 +18,9 @@ export const ActionsCreateSchema = createInsertSchema(actions).omit({
 })
 export type ActionsCreatePayload = z.infer<typeof ActionsCreateSchema>
 
-export const ActionsUpdateSchema = createUpdateSchema(actions).omit({
+export const ActionsUpdateSchema = createUpdateSchema(actions, {
+  order: orderSchema.optional(),
+}).omit({
   id: true,
   workspaceId: true,
   createdAt: true,
