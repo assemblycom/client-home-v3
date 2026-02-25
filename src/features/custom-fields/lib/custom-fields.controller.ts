e6b764f99cdcca829ff543325b8bc15b7982c192
priosshrsth <shrsthprios@hotmail.com>
2026-02-25 07:50:34 +0000

    feat(out-3192): add new endpoints to fetch custom fields

--- src/features/custom-fields/lib/custom-fields.controller.ts
+++ src/features/custom-fields/lib/custom-fields.controller.ts
@@ -3,26 +3,10 @@ import { authenticateHeaders } from '@auth/lib/authenticate'
 import { type NextRequest, NextResponse } from 'next/server'
 import type { APIResponse } from '@/app/types'
 
-export const getCompanyCustomFields = async (
-  req: NextRequest,
-  { params }: { params: Promise<{ entityType: string }> },
-): Promise<NextResponse<APIResponse>> => {
+export const getCompanyCustomFields = async (req: NextRequest): Promise<NextResponse<APIResponse>> => {
   const user = authenticateHeaders(req.headers)
 
   const assembly = new AssemblyClient(user.token)
-
-  const { entityType } = await params
-
-  if (entityType !== 'company' && entityType !== 'client') {
-    return NextResponse.json(
-      {
-        message: 'Path not found.',
-      },
-      {
-        status: 404,
-      },
-    )
-  }
 
   const response = await assembly.getCompanyCustomFields()
 
