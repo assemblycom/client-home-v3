e16bd50d0c53a01313838412213a6774325b92d8
Rojan Rajbhandari <rrojan20@gmail.com>
2026-02-26 18:48:39 +0545

    feat(OUT-3211): implement deep linking in view store

--- src/features/editor/components/ClientEditorWrapper.tsx
+++ src/features/editor/components/ClientEditorWrapper.tsx
@@ -2,8 +2,11 @@
 
 import { useAuthStore } from '@auth/providers/auth.provider'
 import { ReadonlyEditor } from '@editor/components/Editor/ReadonlyEditor'
+import { useViewStore } from '@editor/stores/viewStore'
 import { useSettingsStore } from '@settings/providers/settings.provider'
+import { useQuery } from '@tanstack/react-query'
 import { ActionsCard } from '@/features/action-items/components/actions-card'
+import { api } from '@/lib/core/axios.instance'
 import { Heading } from './Heading'
 import { Subheading } from './Subheading'
 
@@ -11,6 +14,20 @@ export const ClientEditorWrapper = () => {
   const token = useAuthStore((store) => store.token)
   const content = useSettingsStore((store) => store.content)
   const backgroundColor = useSettingsStore((store) => store.backgroundColor)
+  const setTasksAppId = useViewStore((store) => store.setTasksAppId)
+
+  useQuery({
+    queryKey: ['tasks-app-id'],
+    queryFn: async (): Promise<string> => {
+      const res = await api.get<{ data: string }>(`/api/workspace/tasks-app-id?token=${token}`)
+      setTasksAppId(res.data.data)
+      return res.data.data
+    },
+    refetchInterval: 0,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+  })
 
   return (
     <div className={`@container min-h-full w-full overflow-auto px-12 py-11`} style={{ backgroundColor }}>
--- src/features/editor/stores/viewStore.ts
+++ src/features/editor/stores/viewStore.ts
@@ -15,18 +15,21 @@ interface ViewStoreState {
   viewMode: ViewMode
   displayMode: DisplayMode
   workspace: WorkspaceResponse | null
+  tasksAppId: string | null
 }
 
 interface ViewStoreAction {
   reset: () => void
   changeView: (data: Partial<ViewStoreState>) => void
   setWorkspace: (workspace: WorkspaceResponse) => void
+  setTasksAppId: (id: string) => void
 }
 
 const defaultState = {
   viewMode: ViewMode.EDITOR,
   displayMode: DisplayMode.DESKTOP,
   workspace: null,
+  tasksAppId: null,
 } as const satisfies Partial<ViewStoreState>
 
 type ViewStore = ViewStoreState & ViewStoreAction
@@ -35,6 +38,7 @@ export const useViewStore = create<ViewStore>()((set) => ({
   ...defaultState,
   changeView: (data: Partial<ViewStoreState>) => set(data),
   setWorkspace: (workspace: WorkspaceResponse) => set({ workspace }),
+  setTasksAppId: (id: string) => set({ tasksAppId: id }),
   reset: () => set(defaultState),
 }))
 
