a0faa13d343303b69f64b9a04df846b52ef121b3
arpandhakal <arpandhakal20@gmail.com>
2026-03-04 20:08:08 +0545

    feat(OUT-2850): applied default content to be applied on the first install

--- src/features/settings/constants.ts
+++ src/features/settings/constants.ts
@@ -1,3 +1,36 @@
-export const defaultContent = `<p>Hello world!</p>`
+export const defaultContent = `<callout>ℹ️ This page is an example. Edit the content and add a banner image in your Client Home app to give your clients a personalized experience when they open up the client portal. The action items widget will show client notifications for actions they have to take (in real-time), if they have any.</callout>
+<p></p>
+<h1>Welcome, <autofill-field data-value="{{client.firstName}}"></autofill-field></h1>
+<p>Welcome to your <autofill-field data-value="{{workspace.brand}}"></autofill-field> client portal.</p>
+<p></p>
+<p>This portal is your all-in-one experience while working with <autofill-field data-value="{{workspace.brand}}"></autofill-field> where you can view and complete important tasks and connect with our team.</p>
+<p></p>
+<p>Here are our Working Hours:</p>
+<p></p>
+<table><tbody><tr><th colspan="1" rowspan="1"><p>Days</p></th><th colspan="1" rowspan="1"><p>Hours (EST)</p></th></tr><tr><td colspan="1" rowspan="1"><p>M-F</p></td><td colspan="1" rowspan="1"><p>9AM - 6PM</p></td></tr><tr><td colspan="1" rowspan="1"><p>Sat</p></td><td colspan="1" rowspan="1"><p>10AM - 5PM</p></td></tr><tr><td colspan="1" rowspan="1"><p>Sun</p></td><td colspan="1" rowspan="1"><p>Closed</p></td></tr></tbody></table>
+<p></p>
+<p></p>
+<h1>How to use the <autofill-field data-value="{{workspace.brand}}"></autofill-field> portal</h1>
+<p>Save the link to this portal. If you miss an email notification, you can always log in and check this portal for reminders on important tasks and actions required. Explore the options on your left sidebar. We may add more options tailored to your experience.</p>
+<p></p>
+<h2>💬 Messages</h2>
+<p>Questions? Chat with the <autofill-field data-value="{{workspace.brand}}"></autofill-field> team and get answers.</p>
+<hr>
+<callout>⚠️ Apps below should show if they are enabled in the workspace. Ignore for now.</callout>
+<p></p>
+<h2>📂 Files</h2>
+<p>Use this tab to share files with the <autofill-field data-value="{{workspace.brand}}"></autofill-field> team — you can upload any required documents and assets here. This is where we will store files for reference in the future.</p>
+<p></p>
+<h2>📜 Contracts</h2>
+<p>View and sign contracts shared by our team.</p>
+<p></p>
+<h2>📤 Forms</h2>
+<p>Fill out forms with all relevant information.</p>
+<p></p>
+<h2>💵 Billing</h2>
+<p>Here you will see any active subscriptions you have and any invoices after we send them to you.</p>
+<p></p>
+<h2>🆘 Helpdesk</h2>
+<p>Access a collection of helpful articles we have put together.</p>`
 
 export const SETTINGS_QUERY_KEY = 'settings'
