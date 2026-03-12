2d4a4236ee2f088632f8a7736f71b19e2584bcc6
priosshrsth <shrsthprios@hotmail.com>
2026-03-12 16:59:09 +0000

    fix: test vercel url

--- src/config/env.ts
+++ src/config/env.ts
@@ -20,5 +20,6 @@ const EnvSchema = z.object({
 
 export default EnvSchema.parse({
   ...process.env,
+  VERCEL_URL: `https://${process.env.VERCEL_URL}`,
   // VERCEL_URL: `${['production', 'preview'].includes(process.env.VERCEL_ENV || '') ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : process.env.VERCEL_URL || 'http://localhost:3000'}`,
 })
